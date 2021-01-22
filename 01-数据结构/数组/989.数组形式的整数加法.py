91d232b9c386bd8ead45f76ee7da43325d5b147c
liujianhan <liujianhan@gmail.com>
2021-01-22 10:35:08 +0800

    add new ac.

---
+++ 01-数据结构/数组/989.数组形式的整数加法.py
@@ -0,0 +1,54 @@
+"""
+  @Author       : liujianhan
+  @Date         : 2021/1/22 10:11
+  @Project      : leetcode_in_python
+  @FileName     : 989.数组形式的整数加法.py
+  @Description  : 对于非负整数 X 而言，X 的数组形式是每位数字按从左到右的顺序形成的数组。例如，如果 X = 1231，那么其数组形式为 [1,2,3,1]。
+    给定非负整数 X 的数组形式 A，返回整数 X+K 的数组形式。
+     
+    示例 1：
+    输入：A = [1,2,0,0], K = 34
+    输出：[1,2,3,4]
+    解释：1200 + 34 = 1234
+
+    示例 2：
+    输入：A = [2,7,4], K = 181
+    输出：[4,5,5]
+    解释：274 + 181 = 455
+
+    示例 3：
+    输入：A = [2,1,5], K = 806
+    输出：[1,0,2,1]
+    解释：215 + 806 = 1021
+
+    示例 4：
+    输入：A = [9,9,9,9,9,9,9,9,9,9], K = 1
+    输出：[1,0,0,0,0,0,0,0,0,0,0]
+    解释：9999999999 + 1 = 10000000000
+
+    提示：
+    1 <= A.length <= 10000
+    0 <= A[i] <= 9
+    0 <= K <= 10000
+    如果 A.length > 1，那么 A[0] != 0
+"""
+from typing import List
+
+
+class Solution:
+    # 328ms, 15.8MB
+    @staticmethod
+    def add_to_array_form(A: List[int], K: int) -> List[int]:
+        integer = int(''.join(map(str, A))) + K
+        return list(map(int, list(str(integer))))
+
+
+if __name__ == '__main__':
+    test_cases = [
+        ([1, 2, 0, 0], 34),
+        ([2, 7, 4], 181),
+        ([2, 1, 5], 806),
+        ([9, 9, 9, 9, 9, 9, 9, 9, 9, 9], 1)
+    ]
+    for tc in test_cases:
+        print(Solution.add_to_array_form(*tc))
